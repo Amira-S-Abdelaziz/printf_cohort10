5204147e17f4a86fb58181fa503706d8a5c1c79b
MidoriyaArima3451 <113601076+MidoriyaArima3451@users.noreply.github.com>
2022-11-26 01:41:58 +0200

    here it is

--- _pr.c
+++ _pr.c
@@ -24,26 +24,11 @@ int _printf(const char *format, ...)
 		{
 			if (*(format + formsize + 1) == 'c')
 			{
-<<<<<<< HEAD
 				c(va_arg(arguat, int), i++;
 			}
 			else
 			{
 				i = s(va_arg(arguat, char *));
-=======
-				tempc = va_arg(arguat, int);
-				_putchar((char)tempc);
-				i++;
-			}
-			else
-			{
-			strcpy(temp, va_arg(arguat, char*));
-			while (*(temp + i) != '\0')
-			{
-				_putchar(*(temp + i));
-				i++;
-			}
->>>>>>> afac56125047f44d364a8ec3652d035c54faa3f5
 			}
 			len += i;
 			i = 0;
@@ -59,12 +44,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-<<<<<<< HEAD
 		_putchar(*(format + formsize)), len++;
-=======
-			_putchar(*(format + formsize));
-			len++;
->>>>>>> afac56125047f44d364a8ec3652d035c54faa3f5
 		}
 		formsize++;
 	}
