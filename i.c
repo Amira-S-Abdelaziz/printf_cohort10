0a55ac783f16277b58eb39f59d956b4911924979
Amira_Elsayed <e.amira67@gmail.com>
2022-11-25 17:15:57 -0800

    last

--- i.c
+++ i.c
@@ -10,11 +10,25 @@
 int iord(long int a)
 {
 	int sz = 0;
+	int i = 0;
+	char *s;
+	long int a2 = a;
 
 	do {
-		_putchar(a % 10 + '0');
 		sz++;
 		a = a / 10;
 	} while (a != 0);
+	s = (char *) malloc(sizeof(char) * sz);
+	do {
+		*(s + i) = (a2 % 10) + '0';
+		i++;
+		a2 = a2 / 10;
+	} while (a2 != 0);
+	i--;
+	while (i != -1)
+	{
+		_putchar(*(s + i));
+		i--;
+	}
 	return (sz);
 }
